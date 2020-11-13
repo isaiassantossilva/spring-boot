20c01c2a208eba1b60b8199f1bfc9e97c2673de8
isaiassantossilva <isaias661248@gmail.com>
2020-11-13 15:51:07 -0300

    first commit

---
+++ src/main/java/com/santos/springbool/SpringBoolApplication.java
@@ -0,0 +1,13 @@
+package com.santos.springbool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/santos/springbool/SpringBoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.santos.springbool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
