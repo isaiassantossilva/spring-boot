db0a92b278fb274d1ef4b3f4db2c2f44227d9b71
isaiassantossilva <isaias661248@gmail.com>
2020-11-19 20:36:58 -0300

    obtendo post por id

---
+++ src/main/java/com/santos/springbool/services/PostService.java
@@ -0,0 +1,22 @@
+package com.santos.springbool.services;
+
+import java.util.Optional;
+
+import com.santos.springbool.domain.Post;
+import com.santos.springbool.repository.PostRepository;
+import com.santos.springbool.services.exception.ObjectNotFoundException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PostService {
+    
+    @Autowired
+    private PostRepository repo;
+
+    public Post findById(String id) {
+        Optional<Post> post = repo.findById(id);
+        return post.orElseThrow(() -> new ObjectNotFoundException("objeto não encontrado"));
+    }
+}
