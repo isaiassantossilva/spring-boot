f6273f403339927229dbd801ca27e7de457f97ef
isaiassantossilva <isaias661248@gmail.com>
2020-11-19 17:03:03 -0300

    insercao de usuario com post

--- src/main/java/com/santos/springbool/services/UserService.java
+++ src/main/java/com/santos/springbool/services/UserService.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import com.santos.springbool.domain.User;
+import com.santos.springbool.dto.UserDTO;
 import com.santos.springbool.repository.UserRepository;
 import com.santos.springbool.services.exception.ObjectNotFoundException;
 
@@ -23,6 +24,14 @@ public class UserService {
     public User findById(String id){
         Optional<User> obj = repo.findById(id);
         return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+    }
+
+    public User insert(User obj){
+        return repo.insert(obj);
+    }
+
+    public User fromDTO(UserDTO objDto) {
+        return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
     }
 
 }
