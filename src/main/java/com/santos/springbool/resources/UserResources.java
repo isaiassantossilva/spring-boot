24eeee8d7e9c20f4789e9b0ba63c4681eac402b6
isaiassantossilva <isaias661248@gmail.com>
2020-11-19 18:57:01 -0300

    atualizacao de usuario com PUT

--- src/main/java/com/santos/springbool/resources/UserResources.java
+++ src/main/java/com/santos/springbool/resources/UserResources.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -54,6 +55,14 @@ public class UserResources {
     @DeleteMapping(value = "/{id}")
     public ResponseEntity<Void> deleteById(@PathVariable String id) {
         service.deleteById(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<Void> update(@PathVariable String id, @RequestBody UserDTO objDto) {
+        User user = service.fromDTO(objDto);
+        user.setId(id);
+        service.update(user);
         return ResponseEntity.noContent().build();
     }
 
