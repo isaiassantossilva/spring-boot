591897da647a60de6d1b2690f28270b53161ba8c
isaiassantossilva <isaias661248@gmail.com>
2020-11-19 19:43:20 -0300

    criando entity post com user aninhado

--- src/main/java/com/santos/springbool/config/Instanciation.java
+++ src/main/java/com/santos/springbool/config/Instanciation.java
@@ -1,8 +1,12 @@
 package com.santos.springbool.config;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.TimeZone;
 
+import com.santos.springbool.domain.Post;
 import com.santos.springbool.domain.User;
+import com.santos.springbool.repository.PostRepository;
 import com.santos.springbool.repository.UserRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,18 +18,29 @@ import org.springframework.context.annotation.Configuration;
 public class Instanciation implements CommandLineRunner {
 
     @Autowired
-    private UserRepository repo;
+    private UserRepository userRepo;
+
+    @Autowired
+    private PostRepository postRepo;
 
     @Override
     public void run(String... args) throws Exception {
 
-        repo.deleteAll();
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/mm/yyyy");
+        sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
+
+        userRepo.deleteAll();
+        postRepo.deleteAll();
 
         User maria = new User(null, "Maria Brown", "maria@gmail.com");
         User alex = new User(null, "Alex Green", "alex@gmail.com");
         User bob = new User(null, "Bob Grey", "bob@gmail.com");
 
-        repo.saveAll(Arrays.asList(maria, alex, bob));
+        Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para são Paulo", maria);
+        Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje", maria);
+
+        userRepo.saveAll(Arrays.asList(maria, alex, bob));
+        postRepo.saveAll(Arrays.asList(post1, post2));
     }
     
 }
---
+++ src/main/java/com/santos/springbool/repository/PostRepository.java
@@ -0,0 +1,11 @@
+package com.santos.springbool.repository;
+
+import com.santos.springbool.domain.Post;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String> {
+    
+}
