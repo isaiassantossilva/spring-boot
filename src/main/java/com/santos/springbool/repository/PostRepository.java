f1c21387ad0f3c01c177adbe39c5869fd5d23fb3
isaiassantossilva <isaias661248@gmail.com>
2020-11-19 22:03:37 -0300

    consulta simples com @Query

--- src/main/java/com/santos/springbool/repository/PostRepository.java
+++ src/main/java/com/santos/springbool/repository/PostRepository.java
@@ -5,9 +5,14 @@ import java.util.List;
 import com.santos.springbool.domain.Post;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
+
+    @Query("{'title': { $regex: ?0, $options: 'i'}}")
+    List<Post> searchTitle(String title);
+
     List<Post> findByTitleContainingIgnoreCase(String text);
 }
